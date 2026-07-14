f2879c7b1863de53bbf25c43841d706d93db1140
pgzbc001 <41930066+pgzbc001@users.noreply.github.com>
2026-07-14 21:13:56 +0800

    Initial Commit

---
+++ Timber/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Timber
+//
+//  Created by cwenbo on 2026/7/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
